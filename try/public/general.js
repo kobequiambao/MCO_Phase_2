99967151512237c59c0ba2b2b23b31edb2f2a8a8
MichaelGelo <gelodepasucat10@gmail.com>
2024-03-09 19:36:54 +0800

    init

--- try/public/general.js
+++ try/public/general.js
@@ -1,6 +1,6 @@
     
     function navigateToPost(url) {
-        if(!event.target.classList.contains('up_vote') && !event.target.classList.contains('down_vote')) {
+        if(!event.target.classList.contains('up_vote') && !event.target.classList.contains('down_vote') && !event.target.classList.contains('delete-post-btn')) {
             window.location.href = url;
         }
       }
@@ -40,4 +40,17 @@
         }
       }
 
-  
+      function openForm() {
+            
+        document.getElementById("popup").style.display = "block";
+      }
+      
+      function closeForm() {
+        document.getElementById("popup").style.display = "none";
+      }
+
+      function handleDelete(event) {
+        event.stopPropagation();
+        openForm();
+    }
+    
