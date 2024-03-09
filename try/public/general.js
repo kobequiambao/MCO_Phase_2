84bdb30265c58ad5236b2f5a645503d37447dd10
MichaelGelo <gelodepasucat10@gmail.com>
2024-03-09 09:25:56 +0800

    init

--- try/public/general.js
+++ try/public/general.js
@@ -60,3 +60,25 @@ function toggleDropdown_search() {
     dropdownContent.classList.toggle('show_search');
 }
 
+// ------------------------------------------------------------------
+function openLoginForm() {
+    var modal = document.getElementById("loginFormModal");
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // Disable scrolling on the body
+}
+
+function closeLoginForm() {
+    var modal = document.getElementById("loginFormModal");
+    modal.style.display = "none";
+    document.body.style.overflow = ""; // Re-enable scrolling on the body
+}
+
+  
+  // Close modal if clicked outside of it
+  window.onclick = function(event) {
+    var modal = document.getElementById("loginFormModal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
