59f7d939b04bc824eed3199f9cb262162caabc69
bas10agu <alfredbastin.agustines04@gmail.com>
2024-03-11 01:06:11 +0800

    mongo start

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser')
 server.use(express.json()); 
@@ -12,6 +13,68 @@ server.engine('hbs', handlebars.engine({
 }));
 
 server.use(express.static('public'));
+
+// mongoose stuff
+mongoose.connect("mongodb://localhost:27017/MCO2", {useNewUrlParser: true});
+
+const AccountSchema = new mongoose.Schema({
+    bio: String,
+    college: String,
+    email: String,
+    idNo: Number,
+    isAdmin: Boolean,
+    password: String,
+    photo: Buffer,
+    username: String,
+});
+
+const CommentInfoSchema = new mongoose.Schema({
+    Body: String,
+    Date: String,
+    PostId: String,
+    isHidden: Boolean,
+    CommenterId: String,
+    username: Number,
+});
+
+const HiddenSchema = new mongoose.Schema({
+    AccountId: String,
+    PostId: String,
+});
+
+const PostInfoSchema = new mongoose.Schema({
+    Body: String,
+    College: String,
+    Title: String,
+    Type: String,
+    CommentCount: Number,
+    isHidden: Boolean,
+    AccountId: String,
+    NumvoteCount: Number,
+    Date: String,
+});
+
+const ReplyInfoSchema = new mongoose.Schema({
+   Body: String,
+   CommentId: String,
+   Date: String,
+   isHidden: Boolean,
+   NumvoteCount: Number,
+   CommenterId: String, 
+});
+
+const Account = mongoose.model('Account', AccountSchema);
+const CommentInfo = mongoose.model('CommentInfo', CommentInfoSchema);
+const Hidden = mongoose.model('Hidden', HiddenSchema);
+const PostInfo = mongoose.model('PostInfo', PostInfoSchema);
+const ReplyInfo = mongoose.model('ReplyInfo', ReplyInfoSchema);
+
+
+
+
+// until here
+
+
 
 server.get('/', function(req, resp){
     resp.render('main',{
@@ -26,7 +89,7 @@ server.get('/login', function(req, resp){
         title: 'Log In'
     });
 });
-//sdfsdfsdfsdfs
+
 server.get('/general', function(req, resp){
     resp.render('general',{
         layout: 'index',
@@ -59,13 +122,6 @@ server.get('/editProfile', function(req, resp){
     resp.render('editProfile',{
         layout: 'index',
         title: 'Edit Profile'
-    });
-});
-
-server.get('/main', function(req, resp){
-    resp.render('main',{
-        layout: 'index',
-        title: 'Main'
     });
 });
 
